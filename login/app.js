1a7b62867b03188c0c1169533d9fc051d8083986
dezhenglu <89102698@qq.com>
2017-02-15 16:47:01 +0800

    second commit

---
+++ login/app.js
@@ -0,0 +1,17 @@
+App({
+  onLaunch: function () {
+    
+  },
+  onShow: function () {
+    
+  },
+  onHide: function () {
+    
+  },
+  onError: function (msg) {
+    
+  },
+  appData:{
+    userinfo:null
+  }
+})
